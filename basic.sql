83f0f87997ef6c92caa042e3fee761df572660f1
wisdom <wisdom0405@kakao.com>
2024-07-19 17:45:27 +0900

    update

---
+++ basic.sql
@@ -0,0 +1,4 @@
+create table member(id bigint auto_increment,
+                    name varchar(255), email varchar(255),
+                    password varchar(255), primary key(id)
+                    );
